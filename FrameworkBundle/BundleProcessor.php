aecc7d5f5d3e25bd1aa59823f738cf4471f5825b
Tomáš Jančar <jan02@modpreneur.com>
2015-07-08 09:50:07 +0200

    trinity - composer update

--- FrameworkBundle/BundleProcessor.php
+++ FrameworkBundle/BundleProcessor.php
@@ -2,6 +2,7 @@
 
 namespace Trinity\FrameworkBundle;
 
+use Doctrine\Common\Cache\PhpFileCache;
 use Symfony\Component\Finder\Finder;
 use Symfony\Component\Yaml\Dumper;
 use Symfony\Component\Yaml\Parser;
@@ -10,10 +11,10 @@ use Symfony\Component\Yaml\Parser;
 //@todo
 class BundleProcessor
 {
-    private static function endsWith($haystack, $needle) {
+    private static function endsWith($haystack, $needle)
+    {
         return $needle === "" || (($temp = strlen($haystack) - strlen($needle)) >= 0 && strpos($haystack, $needle, $temp) !== FALSE);
     }
-
 
 
     /**
@@ -22,7 +23,8 @@ class BundleProcessor
      * @param $ymlPath
      * @return array
      */
-    private static function parseYML($ymlPath){
+    private static function parseYML($ymlPath)
+    {
         $yml = new Parser();
         $values = $yml->parse(file_get_contents($ymlPath));
 
@@ -30,24 +32,23 @@ class BundleProcessor
     }
 
 
-
     /**
      * @param array $sources
      * @return Finder
      */
-    private static function getFinder(array $sources){
-        $finder     = new Finder();
+    private static function getFinder(array $sources)
+    {
+        $finder = new Finder();
 
         $finder = $finder->files();
 
-        foreach($sources as $source){
+        foreach ($sources as $source) {
             $finder = $finder->in($source);
         }
 
         $finder->name('*Bundle.php');
         return $finder;
     }
-
 
 
     /**
@@ -57,15 +58,15 @@ class BundleProcessor
      * @param $sources
      * @return array
      */
-    public static function getInstalledBundles($sources){
+    public static function getInstalledBundles($sources)
+    {
         $installedBundles = [];
-        foreach(self::getFinder($sources) as $file){
+        foreach (self::getFinder($sources) as $file) {
             /** @var $file \Symfony\Component\Finder\SplFileInfo */
             $installedBundles[] = $file->getRelativePath();
         }
         return $installedBundles;
     }
-
 
 
     /**
@@ -75,11 +76,12 @@ class BundleProcessor
      * @param $ymlPath
      * @return array
      */
-    public static function getActivedBundles($ymlPath){
+    public static function getActivedBundles($ymlPath)
+    {
         $activeBundles = [];
         $values = self::parseYML($ymlPath);
 
-        if(isset($values["bundles"])){
+        if (isset($values["bundles"])) {
             $activeBundles = $values["bundles"];
         }
 
@@ -87,27 +89,27 @@ class BundleProcessor
     }
 
 
-
     /**
      * Return bundles classes
      * @param $ymlPath
      * @param $sources
+     * @param $cacheDir
      * @return array
      */
-    public static function loadBundles($ymlPath, $sources, $cacheDir){
-        $cache = new \Doctrine\Common\Cache\PhpFileCache($cacheDir);
-        $loadedBundles = [];
+    public static function loadBundles($ymlPath, $sources, $cacheDir)
+    {
+        $cache = new PhpFileCache($cacheDir);
 
-        if(!$cache->contains("bundles")){
+        if (!$cache->contains("bundles")) {
             $loadedBundles = self::prepareBundlesList(self::getActivedBundles($ymlPath), self::getFinder($sources), $ymlPath);
             $cache->save("bundles", $loadedBundles);
-        }else{
+        } else {
             $loadedBundles = $cache->fetch("bundles");
         }
 
         $loadedBundlesClass = [];
 
-        foreach($loadedBundles as $bundle){
+        foreach ($loadedBundles as $bundle) {
             $loadedBundlesClass[] = new $bundle();
         }
 
@@ -115,36 +117,36 @@ class BundleProcessor
     }
 
 
-
     /**
      * @param $ymlPath
      * @param $sources
      * @return array [id, name, status]
      */
-    public static function getBundleList($ymlPath, $sources){
+    public static function getBundleList($ymlPath, $sources)
+    {
         $bundles = [];
-        $activeBundles    = self::getActivedBundles($ymlPath);
+        $activeBundles = self::getActivedBundles($ymlPath);
         $installedBundles = self::getInstalledBundles($sources);
 
         $id = 0;
 
-        foreach($installedBundles as $ib){
+        foreach ($installedBundles as $ib) {
 
             $status = "Disable";
-            $path   = "";
+            $path = "";
 
-            foreach($activeBundles as $ab){
+            foreach ($activeBundles as $ab) {
                 $ex = explode("\\", $ab);
-                if( self::endsWith($ex[count($ex) - 1], $ib) ){
+                if (self::endsWith($ex[count($ex) - 1], $ib)) {
                     $status = "Active";
                     $path = $ab;
                 }
             }
 
             $bundles[] = [
-                "id"     => ++$id,
-                "path"   => $path,
-                "name"   => $ib,
+                "id" => ++$id,
+                "path" => $path,
+                "name" => $ib,
                 "status" => $status,
                 "active" => ($status == "Active")
             ];
@@ -154,9 +156,10 @@ class BundleProcessor
     }
 
 
-    public static function disableBundle($bundlePath, $ymlPath){
-        $values  = self::parseYML($ymlPath);
-        if(($key = array_search($bundlePath, $values["bundles"])) !== false) {
+    public static function disableBundle($bundlePath, $ymlPath)
+    {
+        $values = self::parseYML($ymlPath);
+        if (($key = array_search($bundlePath, $values["bundles"])) !== false) {
             unset($values["bundles"][$key]);
         }
 
@@ -166,27 +169,26 @@ class BundleProcessor
     }
 
 
-
     /**
      * @param $activeBundles
      * @param $finder
      * @param $ymlPath
      * @return array
      */
-    private static function prepareBundlesList($activeBundles, $finder, $ymlPath){
+    private static function prepareBundlesList($activeBundles, $finder, $ymlPath)
+    {
         $bundles = [];
         $ac = self::getActivedBundles($ymlPath);
-        foreach($ac as $bundle){
+        foreach ($ac as $bundle) {
             $bundles[] = $bundle; // edit
         }
         return $bundles;
     }
 
 
-
     public static function activeBundle($bundlePath, $ymlPath)
     {
-        $values  = self::parseYML($ymlPath);
+        $values = self::parseYML($ymlPath);
         $values["bundles"][] = $bundlePath;
 
         $dumper = new Dumper();
