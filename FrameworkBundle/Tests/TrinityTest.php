3b93692bebf29a94599219bcbc8fcef1ef5c6dbc
Tomáš Jančar <jan02@modpreneur.com>
2015-08-03 14:33:41 +0200

    remove dynamic bundles loader

---
+++ FrameworkBundle/Tests/TrinityTest.php
@@ -0,0 +1,13 @@
+<?php
+
+
+/**
+ * Class TrinityTest
+ */
+class TrinityTest extends \Braincrafted\Bundle\TestingBundle\Test\WebTestCase
+{
+    public function testEmptyTest(){
+        $this->assertTrue(TRUE);
+    }
+
+}
