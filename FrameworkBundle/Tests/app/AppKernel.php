d8413ffb97b3bc8f26b2b6852d1ed41b3e00d67d
Tomáš Jančar <jan02@modpreneur.com>
2016-01-26 21:27:14 +0100

    fix test.

--- FrameworkBundle/Tests/app/AppKernel.php
+++ FrameworkBundle/Tests/app/AppKernel.php
@@ -18,6 +18,7 @@ class AppKernel extends Kernel
         return array(
             new \Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
             new \Trinity\FrameworkBundle\TrinityFrameworkBundle(),
+            new \Trinity\Bundle\SettingsBundle\SettingsBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle(),
             new \Symfony\Bundle\SecurityBundle\SecurityBundle(),
