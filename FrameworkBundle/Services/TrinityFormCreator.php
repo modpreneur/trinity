af49721c90f058e26a87f374117181f22c55c465
Jakub Fajkus <jakub.fajkus@gmail.com>
2016-01-30 14:10:04 +0100

    Update TrinityFormCreator.php

--- FrameworkBundle/Services/TrinityFormCreator.php
+++ FrameworkBundle/Services/TrinityFormCreator.php
@@ -6,7 +6,6 @@ namespace Trinity\FrameworkBundle\Services;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormFactoryInterface;
 use Symfony\Component\Form\FormInterface;
-use Symfony\Component\Form\FormTypeInterface;
 use Symfony\Component\Routing\RouterInterface;
 
 class TrinityFormCreator
@@ -33,26 +32,25 @@ class TrinityFormCreator
     /**
      * Creates a form to edit entity.
      *
-     * @param object            $entity             The entity
-     * @param FormTypeInterface $entityType         The entity type object
-     * @param string            $urlPrefix          The entity url name
-     * @param array             $routeParameters    Array of params required to generate URL
-     * @param string            $urlPostfix
-     * @param string            $formCssClasses
-     * @param string            $submitButtonName
-     * @param string            $submitButtonLabel
-     * @param string            $submitButtonClasses
+     * @param object $entity The entity
+     * @param string $entityTypeString
+     * @param string $urlPrefix The entity url name
+     * @param array $routeParameters Array of params required to generate URL
+     * @param array $formOptions
+     * @param string $urlPostfix
+     * @param string $submitButtonName
+     * @param string $submitButtonLabel
+     * @param string $submitButtonClasses
      *
      * @return FormInterface The form
-     * @internal param string $formCssClass
      */
     public function createEditForm(
         $entity,
-        FormTypeInterface $entityType,
+        string $entityTypeString,
         string $urlPrefix,
         array $routeParameters = [],
+        array $formOptions = [],
         string $urlPostfix = "_update",
-        string $formCssClasses = 'edit-form',
         string $submitButtonName = "submit",
         string $submitButtonLabel = "Edit",
         string $submitButtonClasses = "button edit"
@@ -61,14 +59,20 @@ class TrinityFormCreator
     {
         $routeParameters['id'] = $entity->getId();
 
-        $form = $this->formFactory->create(
-            get_class($entityType),
-            $entity,
+        // If the input arrays have the same string keys, then the later value for that key will overwrite the previous one
+        $options = array_merge(
             [
                 'action' => $this->router->generate($urlPrefix.$urlPostfix, $routeParameters),
                 'method' => 'PUT',
-                'attr' => ['class' => $formCssClasses],
-            ]
+                'attr' => ['class' => "edit-form"],
+            ],
+            $formOptions
+        );
+
+        $form = $this->formFactory->create(
+            $entityTypeString,
+            $entity,
+            $options
         );
 
         $form->add(
@@ -87,38 +91,45 @@ class TrinityFormCreator
     /**
      * Creates a form to create entity.
      *
-     * @param object            $entity              The entity
-     * @param FormTypeInterface $entityType          The entity type object
-     * @param string            $urlPrefix           The entity url name
-     * @param string[]          $routeParameters     Array of params required to generate URL
-     * @param string            $urlPostfix
-     * @param string            $formCssClasses
-     * @param string            $submitButtonName
-     * @param string            $submitButtonLabel
-     * @param string            $submitButtonClasses
+     * @param object $entity The entity
+     * @param string $entityTypeString
+     * @param string $urlPrefix The entity url name
+     * @param string[] $routeParameters Array of params required to generate URL
+     * @param array $formOptions
+     * @param string $urlPostfix
+     * @param string $submitButtonName
+     * @param string $submitButtonLabel
+     * @param string $submitButtonClasses
      *
      * @return FormInterface The form
      */
     public function createCreateForm(
         $entity,
-        FormTypeInterface $entityType,
+        string $entityTypeString,
         string $urlPrefix,
         array $routeParameters = [],
+        array $formOptions = [],
         string $urlPostfix = "_create",
-        string $formCssClasses = 'edit-form',
         string $submitButtonName = "submit",
         string $submitButtonLabel = "Edit",
         string $submitButtonClasses = "button edit"
     ):FormInterface
     {
-        $form = $this->formFactory->create(
-            get_class($entityType),
-            $entity,
+
+        // If the input arrays have the same string keys, then the later value for that key will overwrite the previous one
+        $options = array_merge(
             [
                 'action' => $this->router->generate($urlPrefix.$urlPostfix, $routeParameters),
                 'method' => 'POST',
-                'attr' => ['class' => $formCssClasses],
-            ]
+                'attr' => ['class' => "new-form"],
+            ],
+            $formOptions
+        );
+
+        $form = $this->formFactory->create(
+            $entityTypeString,
+            $entity,
+            $options
         );
 
         $form->add(
@@ -137,24 +148,23 @@ class TrinityFormCreator
     /**
      * Creates a form to delete entity by id.
      *
-     * @param string    $urlPrefix       The entity url name
-     * @param int       $id              The entity id
-     * @param string[]  $routeParameters Array of params required to generate URL
-     * @param string    $urlPostfix
-     * @param string    $formCssClasses
-     * @param string    $submitButtonName
-     * @param string    $submitButtonLabel
-     * @param string    $submitButtonClasses
-     * @param string    $submitButtonOnClick
-     *
+     * @param string $urlPrefix The entity url name
+     * @param int $id The entity id
+     * @param string[] $routeParameters Array of params required to generate URL
+     * @param array $formOptions
+     * @param string $urlPostfix
+     * @param string $submitButtonName
+     * @param string $submitButtonLabel
+     * @param string $submitButtonClasses
+     * @param string $submitButtonOnClick
      * @return FormInterface The form
      */
     public function createDeleteForm(
         string $urlPrefix,
         int $id,
         array $routeParameters = [],
+        array $formOptions = [],
         string $urlPostfix = "_delete",
-        string $formCssClasses = 'delete-form',
         string $submitButtonName = "submit",
         string $submitButtonLabel = "Delete",
         string $submitButtonClasses = "button delete",
@@ -163,23 +173,34 @@ class TrinityFormCreator
     {
         $routeParameters['id'] = $id;
 
-        return $this->formFactory->createBuilder(null, ['attr' => ['class' => $formCssClasses]])
-            ->setAction(
-                $this->router->generate($urlPrefix.$urlPostfix, $routeParameters)
-            )
-            ->setMethod(
-                'DELETE'
-            )
-            ->add(
-                $submitButtonName,
-                SubmitType::class,
-                [
-                    'label' => $submitButtonLabel,
-                    'attr' => [
-                        'class' => $submitButtonClasses,
-                        'onclick' => $submitButtonOnClick,
-                    ],
+        // If the input arrays have the same string keys, then the later value for that key will overwrite the previous one
+        $options = array_merge(
+            [
+                'action' => $this->router->generate($urlPrefix.$urlPostfix, $routeParameters),
+                'method' => 'DELETE',
+                'attr' => ['class' => "delete-form"],
+            ],
+            $formOptions
+        );
+
+        $form = $this->formFactory->create(
+            'Symfony\Component\Form\Extension\Core\Type\FormType',
+            null,
+            $options
+        );
+
+        $form->add(
+            $submitButtonName,
+            SubmitType::class,
+            [
+                'label' => $submitButtonLabel,
+                'attr' => [
+                    'class' => $submitButtonClasses,
+                    'onClick' => $submitButtonOnClick
                 ]
-            )->getForm();
+            ]
+        );
+
+        return $form;
     }
 }
