28d119c8c66d07f9a5bfb170f603a255f49d8b91
Ivo <fra01@modpreneur.com>
2016-04-11 10:05:24 +0200

    BraincraftedTestingBundle delete from AppKernel.php by TOM

--- FrameworkBundle/Tests/app/AppKernel.php
+++ FrameworkBundle/Tests/app/AppKernel.php
@@ -21,9 +21,7 @@ class AppKernel extends Kernel
             new \Trinity\Bundle\SettingsBundle\SettingsBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle(),
-            new \Symfony\Bundle\SecurityBundle\SecurityBundle(),
-
-            new Braincrafted\Bundle\TestingBundle\BraincraftedTestingBundle($this),
+            new \Symfony\Bundle\SecurityBundle\SecurityBundle()
         );
     }
 
