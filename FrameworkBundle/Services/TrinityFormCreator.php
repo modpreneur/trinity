5e31dcabb5362643c4a91420e2089e417143f808
Zbynek Fiser <fiser@modpreneur.com>
2016-10-21 17:21:55 +0200

    FormName support ++ each type of form can be created with custom name attribute, new fn for it ++ Utils with mergeArrays deep

--- FrameworkBundle/Services/TrinityFormCreator.php
+++ FrameworkBundle/Services/TrinityFormCreator.php
@@ -7,6 +7,7 @@ use Symfony\Component\Form\FormFactoryInterface;
 use Symfony\Component\Form\FormInterface;
 use Symfony\Component\Routing\RouterInterface;
 use Trinity\Component\Core\Interfaces\EntityInterface;
+use Trinity\FrameworkBundle\Utils\Utils;
 
 /**
  * Class TrinityFormCreator
@@ -68,33 +69,75 @@ class TrinityFormCreator
         string $submitButtonLabel = 'Update',
         string $submitButtonClasses = 'button button-success button-save'
     ):FormInterface {
-        $routeParameters['id'] = $entity->getId();
-        // If the input arrays have the same string keys, then the later value for that key will overwrite the previous one
-        $options = array_merge(
-            [
-                'action' => $this->router->generate($urlPrefix . $urlPostfix, $routeParameters),
-                'method' => 'PUT',
-                'attr' => ['class' => 'edit-form'],
-            ],
-            $formOptions
-        );
-
-        $form = $this->formFactory->create(
-            $entityTypeString,
+        return $this->createNamedEditForm(
             $entity,
-            $options
-        );
-
-        $form->add(
+            $entityTypeString,
+            $urlPrefix,
+            null,
+            $routeParameters,
+            $formOptions,
+            $urlPostfix,
             $submitButtonName,
-            SubmitType::class,
-            [
-                'label' => $submitButtonLabel,
-                'attr' => ['class' => $submitButtonClasses, 'autocomplete' => 'off']
-            ]
+            $submitButtonLabel,
+            $submitButtonClasses
         );
+    }
 
-        return $form;
+    /**
+     * Creates a form to edit entity.
+     *
+     * @param EntityInterface $entity The entity
+     * @param string $entityTypeString
+     * @param string $urlPrefix The entity url name
+     * @param string $formName
+     * @param array $routeParameters Array of params required to generate URL
+     * @param array $formOptions
+     * @param string $urlPostfix
+     * @param string $submitButtonName
+     * @param string $submitButtonLabel
+     * @param string $submitButtonClasses
+     *
+     * @return FormInterface The form
+     *
+     * @throws \Symfony\Component\Form\Exception\UnexpectedTypeException
+     * @throws \Symfony\Component\Form\Exception\LogicException
+     * @throws \Symfony\Component\Form\Exception\AlreadySubmittedException
+     * @throws \Symfony\Component\OptionsResolver\Exception\InvalidOptionsException
+     * @throws \Symfony\Component\Routing\Exception\RouteNotFoundException
+     * @throws \Symfony\Component\Rout$routeParameters['id'] = $entity->getId();ing\Exception\MissingMandatoryParametersException
+     * @throws \Symfony\Component\Routing\Exception\InvalidParameterException
+     */
+    public function createNamedEditForm(
+        $entity,
+        string $entityTypeString,
+        string $urlPrefix,
+        string $formName = '',
+        array $routeParameters = [],
+        array $formOptions = [],
+        string $urlPostfix = '_update',
+        string $submitButtonName = 'submit',
+        string $submitButtonLabel = 'Update',
+        string $submitButtonClasses = 'button button-success button-save'
+    ):FormInterface {
+        $routeParameters['id'] = $entity->getId();
+        return $this->__createNamedForm(
+            $entity,
+            $entityTypeString,
+            $urlPrefix,
+            $formName,
+            $routeParameters,
+            Utils::mergeArraysDeep(
+                [
+                    'method' => 'PUT',
+                    'attr' => ['class' => 'edit-form'],
+                ],
+                $formOptions
+            ),
+            $urlPostfix,
+            $submitButtonName,
+            $submitButtonLabel,
+            $submitButtonClasses
+        );
     }
 
 
@@ -133,20 +176,136 @@ class TrinityFormCreator
         string $submitButtonClasses = 'button button-success button-save'
     ):FormInterface {
         // If the input arrays have the same string keys, then the later value for that key will overwrite the previous one
-        $options = array_merge(
+        return $this->createNamedCreateForm(
+            $entity,
+            $entityTypeString,
+            $urlPrefix,
+            null,
+            $routeParameters,
+            $formOptions,
+            $urlPostfix,
+            $submitButtonName,
+            $submitButtonLabel,
+            $submitButtonClasses
+        );
+    }
+
+
+    /**
+     * Creates a form to create entity.
+     *
+     * @param EntityInterface $entity The entity
+     * @param string $entityTypeString
+     * @param string $urlPrefix The entity url name
+     * @param string $formName Name of form
+     * @param string[] $routeParameters Array of params required to generate URL
+     * @param array $formOptions
+     * @param string $urlPostfix
+     * @param string $submitButtonName
+     * @param string $submitButtonLabel
+     * @param string $submitButtonClasses
+     *
+     * @return FormInterface The form
+     *
+     * @throws \Symfony\Component\Form\Exception\UnexpectedTypeException
+     * @throws \Symfony\Component\Form\Exception\LogicException
+     * @throws \Symfony\Component\Form\Exception\AlreadySubmittedException
+     * @throws \Symfony\Component\OptionsResolver\Exception\InvalidOptionsException
+     * @throws \Symfony\Component\Routing\Exception\RouteNotFoundException
+     * @throws \Symfony\Component\Routing\Exception\MissingMandatoryParametersException
+     * @throws \Symfony\Component\Routing\Exception\InvalidParameterException
+     */
+    public function createNamedCreateForm(
+        $entity,
+        string $entityTypeString,
+        string $urlPrefix,
+        string $formName,
+        array $routeParameters = [],
+        array $formOptions = [],
+        string $urlPostfix = '_create',
+        string $submitButtonName = 'submit',
+        string $submitButtonLabel = 'Create',
+        string $submitButtonClasses = 'button button-success button-save'
+    ):FormInterface {
+        return $this->__createNamedForm(
+            $entity,
+            $entityTypeString,
+            $urlPrefix,
+            $formName,
+            $routeParameters,
+            Utils::mergeArraysDeep(
+                [
+                    'method' => 'POST',
+                    'attr' => ['class' => 'new-form'],
+                ],
+                $formOptions
+            ),
+            $urlPostfix,
+            $submitButtonName,
+            $submitButtonLabel,
+            $submitButtonClasses
+        );
+    }
+
+
+    /**
+     * Creates a form to Edit or Create entity
+     *
+     * @param EntityInterface $entity The entity
+     * @param string $entityTypeString
+     * @param string $urlPrefix The entity url name
+     * @param string $formName Name of form
+     * @param string[] $routeParameters Array of params required to generate URL
+     * @param array $formOptions
+     * @param string $urlPostfix
+     * @param string $submitButtonName
+     * @param string $submitButtonLabel
+     * @param string $submitButtonClasses
+     *
+     * @return FormInterface The form
+     *
+     * @throws \Symfony\Component\Form\Exception\UnexpectedTypeException
+     * @throws \Symfony\Component\Form\Exception\LogicException
+     * @throws \Symfony\Component\Form\Exception\AlreadySubmittedException
+     * @throws \Symfony\Component\OptionsResolver\Exception\InvalidOptionsException
+     * @throws \Symfony\Component\Routing\Exception\RouteNotFoundException
+     * @throws \Symfony\Component\Routing\Exception\MissingMandatoryParametersException
+     * @throws \Symfony\Component\Routing\Exception\InvalidParameterException
+     */
+    private function __createNamedForm(
+        $entity,
+        string $entityTypeString,
+        string $urlPrefix,
+        string $formName = '',
+        array $routeParameters = [],
+        array $formOptions = [],
+        string $urlPostfix = '_create',
+        string $submitButtonName = 'submit',
+        string $submitButtonLabel = 'Create',
+        string $submitButtonClasses = 'button button-success button-save'
+    ):FormInterface {
+        $options = Utils::mergeArraysDeep(
             [
-                'action' => $this->router->generate($urlPrefix . $urlPostfix, $routeParameters),
-                'method' => 'POST',
-                'attr' => ['class' => 'new-form'],
+                'action' => $this->router->generate($urlPrefix . $urlPostfix, $routeParameters)
             ],
             $formOptions
         );
 
-        $form = $this->formFactory->create(
-            $entityTypeString,
-            $entity,
-            $options
-        );
+        $form = null;
+        if($formName === ''){
+            $form = $this->formFactory->create(
+                $entityTypeString,
+                $entity,
+                $options
+            );
+        } else {
+            $form = $this->formFactory->createNamed(
+                $formName,
+                $entityTypeString,
+                $entity,
+                $options
+            );
+        }
 
         $form->add(
             $submitButtonName,
@@ -155,11 +314,11 @@ class TrinityFormCreator
                 'label' => $submitButtonLabel,
                 'attr' => ['class' => $submitButtonClasses, 'autocomplete' => 'off']
             ]
-
         );
 
         return $form;
     }
+
 
 
     /**
@@ -194,10 +353,60 @@ class TrinityFormCreator
         string $submitButtonClasses = 'button button-danger button-remove',
         string $submitButtonOnClick = "return confirm('Are you sure?')"
     ):FormInterface {
+        return $this->createNamedDeleteForm(
+            $urlPrefix,
+            $id,
+            null,
+            $routeParameters,
+            $formOptions,
+            $urlPostfix,
+            $submitButtonName,
+            $submitButtonLabel,
+            $submitButtonClasses,
+            $submitButtonOnClick
+        );
+    }
+
+
+
+    /**
+     * Creates a form to delete entity by id.
+     *
+     * @param string $urlPrefix The entity url name
+     * @param int $id The entity id
+     * @param string $formName
+     * @param string[] $routeParameters Array of params required to generate URL
+     * @param array $formOptions
+     * @param string $urlPostfix
+     * @param string $submitButtonName
+     * @param string $submitButtonLabel
+     * @param string $submitButtonClasses
+     * @param string $submitButtonOnClick
+     * @return FormInterface The form
+     * @throws \Symfony\Component\Form\Exception\UnexpectedTypeException
+     * @throws \Symfony\Component\Form\Exception\LogicException
+     * @throws \Symfony\Component\Form\Exception\AlreadySubmittedException
+     * @throws \Symfony\Component\OptionsResolver\Exception\InvalidOptionsException
+     * @throws \Symfony\Component\Routing\Exception\RouteNotFoundException
+     * @throws \Symfony\Component\Routing\Exception\MissingMandatoryParametersException
+     * @throws \Symfony\Component\Routing\Exception\InvalidParameterException
+     */
+    public function createNamedDeleteForm(
+        string $urlPrefix,
+        int $id,
+        string $formName = '',
+        array $routeParameters = [],
+        array $formOptions = [],
+        string $urlPostfix = '_delete',
+        string $submitButtonName = 'submit',
+        string $submitButtonLabel = 'Delete',
+        string $submitButtonClasses = 'button button-danger button-remove',
+        string $submitButtonOnClick = "return confirm('Are you sure?')"
+    ):FormInterface {
         $routeParameters['id'] = $id;
 
         // If the input arrays have the same string keys, then the later value for that key will overwrite the previous one
-        $options = array_merge(
+        $options = Utils::mergeArraysDeep(
             [
                 'action' => $this->router->generate($urlPrefix . $urlPostfix, $routeParameters),
                 'method' => 'DELETE',
@@ -206,11 +415,21 @@ class TrinityFormCreator
             $formOptions
         );
 
-        $form = $this->formFactory->create(
-            'Symfony\Component\Form\Extension\Core\Type\FormType',
-            null,
-            $options
-        );
+        $form = null;
+        if($formName === '') {
+            $form = $this->formFactory->create(
+                'Symfony\Component\Form\Extension\Core\Type\FormType',
+                null,
+                $options
+            );
+        } else {
+            $form = $this->formFactory->createNamed(
+                $formName,
+                'Symfony\Component\Form\Extension\Core\Type\FormType',
+                null,
+                $options
+            );
+        }
 
         $form->add(
             $submitButtonName,
@@ -228,4 +447,5 @@ class TrinityFormCreator
 
         return $form;
     }
+
 }
---
+++ FrameworkBundle/Utils/Utils.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: fisa
+ * Date: 10/21/16
+ * Time: 3:03 PM
+ */
+
+namespace Trinity\FrameworkBundle\Utils;
+
+
+class Utils
+{
+    /**
+     * Merge deeply two or more arrays into one.
+     * Ordering is from least significant (defaults) to most significant values (our custom behaviour)
+     * @param \array[] ...$arrays unlimited arrays to be merged
+     * @return array
+     */
+    public static function mergeArraysDeep(array ...$arrays): array {
+        $result = [];
+
+        foreach ($arrays as $arr){
+            foreach ($arr as $key => $value){
+                if( array_key_exists($key, $result) && is_array($value) ){
+                    $result[$key] = self::mergeArraysDeep($result[$key], $value);
+                } else {
+                    $result[$key] = $value;
+                }
+            }
+        }
+
+        return $result;
+    }
+}
